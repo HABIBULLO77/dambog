7e8a99622012834c7e612b4af414fd5d36942cc1
Habibullo Hamidov <h.hamidov77777@gmail.com>
2024-09-03 23:56:06 +0900

    feat: developed Admin signup process

--- src/controllers/restraurant.controller.ts
+++ src/controllers/restraurant.controller.ts
@@ -1,28 +1,31 @@
 import { Request, Response } from "express";
 import { T } from "../libs/types/common";
+import { MemberInput } from "../libs/types/member";
+import { MemberType } from "../libs/enums/member.enum";
+import MemberService from "../models/Member.service";
 
 const restaurantController: T = {};
-restaurantController.goHome = (req: Request, res: Response) => {
+
+restaurantController.processSignup = async (req: Request, res: Response) => {
   try {
-    res.send("Home Page");
+    console.log("processSignup");
+    const newMember: MemberInput = req.body;
+    newMember.memberType = MemberType.RESTAURANT;
+
+    const memberService = new MemberService();
+    const result = await memberService.processSignup(newMember);
+    res.send(result);
   } catch (err) {
-    console.log("Error, goHome:", err);
+    console.log("Error, processSignup:", err);
   }
 };
 
-restaurantController.getLogin = (req: Request, res: Response) => {
+restaurantController.processLogin = async (req: Request, res: Response) => {
   try {
-    res.send(" Login Page");
+    console.log("processLogin");
+    res.send("DONE");
   } catch (err) {
-    console.log("Error, getLogin:", err);
-  }
-};
-
-restaurantController.getSignup = (req: Request, res: Response) => {
-  try {
-    res.send(" Signup Page");
-  } catch (err) {
-    console.log("Error, getSignup:", err);
+    console.log("Error, processLogin:", err);
   }
 };
 
